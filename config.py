1cc3ce90161fe5347b0147e0c0982702eb690bfa
Shubham Patil <53473177+shubh480@users.noreply.github.com>
2020-11-06 19:55:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+connection_properties = {
+    'host': 'moviebuff.cmayuty1ln5o.ap-south-1.rds.amazonaws.com',
+    'port': 3306,
+    'user': 'admin',
+    'passwd': 'moviebuffdb',
+    'db':'movietmdb'
+    }
+
+awskey = {
+    'aws_access_key_id':'AKIA2LMVPTSKSR5ATP7H',
+    'aws_secret_access_key':'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'
+}
+
